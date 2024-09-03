687144aef87f29c7153357eb0079a8c42bac4bdb
Aventus <107351932+Aventus007@users.noreply.github.com>
2024-09-03 22:11:09 +0530

    Finished

--- BlackScholes-FrontEnd/src/App.jsx
+++ BlackScholes-FrontEnd/src/App.jsx
@@ -1,12 +1,71 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
+import axios from "axios";
 import './App.css'
 
 function App() {
   const [S, setS] = useState(100.00)
   const [K, setK] = useState(100.00)
-  const [r, setr] = useState(0.10)
-  const [v, setv] = useState(0.50)
+  const [r, setr] = useState(0.05)
+  const [v, setv] = useState(0.20)
   const [T, setT] = useState(1.00)
+  const [callOption, setCallOption] = useState(10.451);
+  const [putOption, setPutOption] = useState(5.574);
+
+  const [minSpotPrice, setminSpotPrice] = useState(80.00)
+  const [maxSpotPrice, setmaxSpotPrice] = useState(120.00)
+  const [minVol, setminVol] = useState(0.01)
+  const [maxVol, setmaxVol] = useState(0.5)
+
+  const [callHeatmap, setCallHeatmap] = useState(null);
+  const [putHeatmap, setPutHeatmap] = useState(null);
+
+  const calculateOptions = async () => {
+    try {
+      // Call the API to get the option prices
+      const response = await axios.post("http://localhost:5000/calculate", {
+        S: parseFloat(S),
+        K: parseFloat(K),
+        r: parseFloat(r),
+        sigma: parseFloat(v),
+        T: parseFloat(T),
+      });
+      setCallOption(response.data["Call Option"]);
+      setPutOption(response.data["Put Option"]);
+    } 
+    catch (error) {
+      console.error("Error calculating options:", error);
+    }
+  };
+
+  const generateHeatmap = async() => {
+    try {
+      // Call the API to get the heatmap
+      const response = await axios.post("http://localhost:5000/heatmap", {
+        minSpotPrice: parseFloat(minSpotPrice),
+        maxSpotPrice: parseFloat(maxSpotPrice),
+        minVol: parseFloat(minVol),
+        maxVol: parseFloat(maxVol),
+        S: parseFloat(S),
+        r: parseFloat(r),
+        T: parseFloat(T),
+      });
+      
+      console.log(response);
+      setCallHeatmap(`data:image/png;base64,${response.data.call_heatmap}`);
+      setPutHeatmap(`data:image/png;base64,${response.data.put_heatmap}`);
+    } 
+    catch (error) {
+      console.error("Error fetching heatmaps:", error);
+    }
+  };
+
+  useEffect(() => {
+    calculateOptions();
+  }, [S, K, r, v, T]);
+
+  useEffect(() => {
+    generateHeatmap();
+  }, [minSpotPrice,maxSpotPrice,minVol,maxVol,S,r,T]);
 
   const handleInputChangeS = (event) => {
     setS(event.target.value);
@@ -27,6 +86,25 @@ function App() {
   const handleInputChangeT = (event) => {
     setT(event.target.value);
   };
+
+  const handleInputChangeMinSpotPrice = (event) => {
+    setminSpotPrice(event.target.value);
+  };
+
+  const handleInputChangeMaxSpotPrice = (event) => {
+    setmaxSpotPrice(event.target.value);
+  };
+
+  const handleInputChangeMinVol = (event) => {
+    setminVol(event.target.value);
+  };
+
+  const handleInputChangeMaxVol = (event) => {
+    setmaxVol(event.target.value);
+  };
+
+
+
 
   return (
     <>
@@ -57,6 +135,25 @@ function App() {
 
         <label htmlFor="maturity" className='label'>Maturity</label>
         <input id="maturity" type="number" defaultValue={T} onChange={handleInputChangeT} className='inpt' min={0}/>
+        <br /><br /><br />
+
+        <hr />
+
+        <div className='heatmap-heading'>HeatMap Parameters</div>
+
+        <label htmlFor="min-spot-price" className='label'>Min Spot Price</label>
+        <input id="min-spot-price" type="number" defaultValue={minSpotPrice} onChange={handleInputChangeMinSpotPrice} className='inpt' min={0}/>
+
+        <label htmlFor="max-spot-price" className='label'>Max Spot Price</label>
+        <input id="max-spot-price" type="number" defaultValue={maxSpotPrice} onChange={handleInputChangeMaxSpotPrice} className='inpt' min={0}/>
+
+        <label htmlFor="min-volatility" className='label'>Min Volatility (0.01 - 1)</label>
+        <input id="min-volatility" type="number" defaultValue={minVol} onChange={handleInputChangeMinVol}
+        className='inpt' step={0.01} min={0.01} max={1}/>
+
+        <label htmlFor="max-volatility" className='label'>Max Volatility (0.01 - 1)</label>
+        <input id="max-volatility" type="number" defaultValue={maxVol} onChange={handleInputChangeMaxVol} className='inpt' step={0.01} min={0.01} max={1}/>
+
       </div>
 
       <div className='content'>
@@ -83,17 +180,25 @@ function App() {
         <div className='container'>
         <div className='call'>
           <div className='call-label'>Call Value</div>
-          <div className='call-value'>$100.00</div>
+          <div className='call-value'>${callOption}</div>
         </div>
 
         <div className='put'>
           <div className='put-label'>Put Value</div>
-          <div className='put-value'>$100.00</div>
+          <div className='put-value'>${putOption}</div>
         </div>
         </div>
 
         <h1 className='heading'>Options Price - Interactive Heatmap</h1>
-        <div className='info'>Explore how option prices fluctuate with varying 'Spot Prices and Volatility' levels using interactive heatmap parameters, all while maintaining a constant 'Strike Price'.</div>        
+        <div className='info'>Explore how option prices fluctuate with varying 'Spot Prices and Volatility' levels using interactive heatmap parameters, all while maintaining a constant 'Strike Price'.</div>
+
+        <div className='heat-map-container'>
+          <div className='call-heat-map'>Call Price HeatMap
+          {callHeatmap && <img src={callHeatmap} alt="Call Heatmap" className='heat-img' />}
+          </div>
+          <div className='put-heat-map'>Put Price HeatMap
+          {putHeatmap && <img src={putHeatmap} alt="Put Heatmap" className='heat-img' />}</div> 
+          </div>       
       </div>
       </div>
     </>
---
+++ app.py
@@ -0,0 +1,102 @@
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+import seaborn as sns
+from flask import Flask, jsonify, request, send_file
+from flask_cors import CORS
+import base64
+import io
+
+plt.switch_backend('Agg')
+
+app = Flask(__name__)
+cors = CORS(app, origins='*',support_credentials=True)
+
+def blackScholes(r, S, K, T, sigma):
+    d1 = (np.log(S/K) + (r + 0.5 * sigma**2)*T)/(sigma*np.sqrt(T))
+
+    d2 = d1 - sigma * np.sqrt(T)
+
+    Call = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+
+    Put = K * np.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+
+    return [round(Call,3),round(Put,3)]
+
+
+def plot_heatmap(spot_range, vol_range, strike, r ,T):
+    call_prices = np.zeros((len(vol_range), len(spot_range)))
+    put_prices = np.zeros((len(vol_range), len(spot_range)))
+    
+    for i, vol in enumerate(vol_range):
+        for j, spot in enumerate(spot_range):
+            temp = blackScholes(r, spot, strike, T, vol)  # Corrected parameter order
+            call_prices[i, j] = temp[0]
+            put_prices[i, j] = temp[1]
+    
+    # Plotting Call Price Heatmap
+    fig_call, ax_call = plt.subplots(figsize=(10, 8))
+    sns.heatmap(call_prices, xticklabels=np.round(spot_range, 2), yticklabels=np.round(vol_range, 2), annot=True, fmt=".2f", cmap="viridis", ax=ax_call)
+    ax_call.set_title('CALL')
+    ax_call.set_xlabel('Spot Price')
+    ax_call.set_ylabel('Volatility')
+    fig_call.tight_layout(pad=1)
+    
+    # Plotting Put Price Heatmap
+    fig_put, ax_put = plt.subplots(figsize=(10, 8))
+    sns.heatmap(put_prices, xticklabels=np.round(spot_range, 2), yticklabels=np.round(vol_range, 2), annot=True, fmt=".2f", cmap="viridis", ax=ax_put)
+    ax_put.set_title('PUT')
+    ax_put.set_xlabel('Spot Price')
+    ax_put.set_ylabel('Volatility')
+    fig_put.tight_layout(pad=1)
+    
+    return fig_call, fig_put
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    data = request.json
+    S = data['S']
+    K = data['K']
+    r = data['r']
+    sigma = data['sigma']
+    T = data['T']
+    call, put = blackScholes(r, S, K, T, sigma)
+    return jsonify({"Call Option": call, "Put Option": put})
+
+@app.route('/heatmap', methods=['POST'])
+def heatmap():
+    data = request.json
+    minSpotPrice = data['minSpotPrice']
+    maxSpotPrice = data['maxSpotPrice']
+    minVol = data['minVol']
+    maxVol = data['maxVol']
+    strike = data['S']
+    r = data['r']
+    T = data['T']
+
+    s_range = np.linspace(minSpotPrice,maxSpotPrice,10)
+    v_range = np.linspace(minVol,maxVol,10)
+
+    fig_call, fig_put = plot_heatmap(s_range, v_range, strike,r,T)
+
+    # Save heatmaps to in-memory files
+    img_call = io.BytesIO()
+    img_put = io.BytesIO()
+    fig_call.savefig(img_call, format='png')
+    fig_put.savefig(img_put, format='png')
+    plt.close(fig_call)
+    plt.close(fig_put)
+    img_call.seek(0)
+    img_put.seek(0)
+
+    # Encode images as Base64
+    img_call_base64 = base64.b64encode(img_call.getvalue()).decode('utf-8')
+    img_put_base64 = base64.b64encode(img_put.getvalue()).decode('utf-8')
+
+    return jsonify({
+        "call_heatmap": img_call_base64,
+        "put_heatmap": img_put_base64
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True,port=5000)
